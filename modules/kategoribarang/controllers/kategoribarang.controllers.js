f25c155b30aa7300449a9269c7e5c1526d450185
Disyam Adityana <disyam.adityana@gmail.com>
2015-01-26 19:37:21 +0700

    rubah create dan update menjadi modal

--- modules/kategoribarang/controllers/kategoribarang.controllers.js
+++ modules/kategoribarang/controllers/kategoribarang.controllers.js
@@ -4,50 +4,31 @@ angular.module('kategoriBarangControllers', [])
 .controller('kategoriBarangController', ['$scope', '$window', '$state', '$stateParams', '$modal', 'kategoriBarangFactory',
 function($scope, $window, $state, $stateParams, $modal, kategoriBarangFactory) {
 	$scope.sort = "kategori";
-	$scope.kategoriBarangs = kategoriBarangFactory.query();
-	$scope.kategoriBarang = new kategoriBarangFactory();
-	$scope.status = $state.$current.data;
-	$scope.create = function() {
+	$scope.load = function(){
+		$scope.kategoriBarangs = kategoriBarangFactory.query();
+		$scope.kategoriBarang = new kategoriBarangFactory;
+	};
+	$scope.close = function(){
+		$scope.modalInstance.dismiss();
+	};
+	$scope.openCreate = function () {
+		$scope.status = true;
+		$scope.modalInstance = $modal.open({
+			templateUrl: 'modules/kategoribarang/views/form-kategoribarang.views.html',
+			size: 'md',
+			backdrop: 'static',
+			scope: $scope
+		});
+	};
+	$scope.create = function(kategoriBarang) {
 		$scope.kategoriBarang.$save(function() {
-			$state.go('listKategoriBarangState');
+			$scope.kategoriBarangs.push(kategoriBarang);
+			$scope.load();
+			$scope.close();
 		});
 	};
-	$scope.update = function() {
-		$scope.kategoriBarang.$update(function() {
-			$state.go('listKategoriBarangState');
-		});
-	};
-	$scope.delete = function(kategoriBarang) {
-		var confirmDelete = $window.confirm('Apakah Anda Yakin?');
-		if ( confirmDelete )
-		{
-			if ( kategoriBarang ) {
-				kategoriBarang.$delete();
-				for (var i in $scope.kategoriBarangs) {
-					if ($scope.kategoriBarangs [i] === kategoriBarang) {
-						$scope.kategoriBarangs.splice(i, 1);
-					};
-				};
-			} else {
-				$scope.kategoriBarang.$delete(function() {
-					$state.go('listKategoriBarangState');
-				});
-			};
-		}
-		else {
-			$state.go('listKategoriBarangState');
-		};
-	};
-	$scope.find = function() {
-		$scope.kategoriBarang = kategoriBarangFactory.get({
-			id: $stateParams.id
-		});
-	};
-	$scope.back = function() {
-		$state.go('listKategoriBarangState');
-	};
-	$scope.openDetail = function (kategoriBarang) {
-		$scope.kategoriBarang = kategoriBarang;
+	$scope.openRead = function (kategoriBarang) {
+		$scope.kategoriBarang = angular.copy(kategoriBarang);
 		$scope.modalInstance = $modal.open({
 			templateUrl: 'modules/kategoribarang/views/read-kategoribarang.views.html',
 			size: 'md',
@@ -55,8 +36,34 @@ function($scope, $window, $state, $stateParams, $modal, kategoriBarangFactory) {
 			scope: $scope
 		});
 	};
-	$scope.closeDetail = function(){
-		$scope.modalInstance.dismiss();
+	$scope.openUpdate = function (kategoriBarang) {
+		$scope.status = false;
+		$scope.kategoriBarangOld = angular.copy(kategoriBarang);
+		$scope.kategoriBarang = angular.copy(kategoriBarang);
+		$scope.modalInstance = $modal.open({
+			templateUrl: 'modules/kategoribarang/views/form-kategoribarang.views.html',
+			size: 'md',
+			backdrop: 'static',
+			scope: $scope
+		});
+	};
+	$scope.update = function(kategoriBarang) {
+		$scope.kategoriBarang.$update(function() {
+			index = $scope.kategoriBarangs.indexOf($scope.kategoriBarangOld);
+			$scope.kategoriBarangs[index] = kategoriBarang;
+			$scope.load();
+			$scope.close();
+		});
+	};
+	$scope.delete = function(kategoriBarang) {
+		var confirmDelete = $window.confirm('Apakah Anda Yakin?');
+		if ( confirmDelete )
+		{
+			kategoriBarang.$delete(function() {
+				$scope.load();
+			});
+		};
+		$state.go('listKategoriBarangState');
 	};
 	$scope.checkAll = function(){
 		angular.forEach($scope.filtered, function(kategoriBarang){
@@ -74,13 +81,9 @@ function($scope, $window, $state, $stateParams, $modal, kategoriBarangFactory) {
 			var i = $scope.kategoriBarangs.length;
 			while (i--) {
 				if ($scope.kategoriBarangs[i].selected){
-					// var status = $scope.kategoriBarangs[i].$delete();
-					// console.log("status : ",status);
-					// status.$promise.then(function (result){
-						// console.log("result : ",result);
-					// })
-					$scope.kategoriBarangs[i].$delete();
-					$scope.kategoriBarangs.splice(i, 1);
+					$scope.kategoriBarangs[i].$delete(function(){
+						$scope.kategoriBarangs.splice(i, 1);
+					});
 				};
 			};
 		};
