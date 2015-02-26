19a73fe76daa60d691bc2c7d8f63864f5379fcf5
Disyam Adityana <disyam.adityana@gmail.com>
2015-02-26 15:27:11 +0700

    pisah kontroller di modul barang

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ angular.module("purchasingApp", [
 ]);
 
 angular.module("purchasing.routes", []).config(function($urlRouterProvider) {
-    $urlRouterProvider.otherwise("/katalogbarang/");
+    $urlRouterProvider.otherwise("/barang/");
 });
--- modules/barang/controllers/barang.controllers.js
+++ modules/barang/controllers/barang.controllers.js
@@ -1,107 +1,136 @@
-angular.module('barang.controllers', []).controller('barangController', ['$scope', '$window', '$state', '$modal', '$filter', 'satuanGudangFactory', 'kategoriBarangFactory', 'barangFactory', function($scope, $window, $state, $modal, $filter, satuanGudangFactory, kategoriBarangFactory, barangFactory) {
-    $scope.module = "barang";
-    $scope.fields = [{
-        "name": "kode",
-        "type": "string",
-        "header": "Kode"
-    }, {
-        "name": "kategori",
-        "type": "string",
-        "header": "kategori"
-    }, {
-        "name": "nama",
-        "type": "string",
-        "header": "Nama"
-    }, {
-        "name": "alias",
-        "type": "string",
-        "header": "Alias"
-    }, {
-        "name": "satuan",
-        "type": "string",
-        "header": "Satuan"
-    }];
-    $scope.Math = window.Math;
-    $scope.sort = {
-        "field": "kode",
-        "order": false
-    };
-    $scope.new = function() {
+angular.module("barang.controllers", []).controller("barangController", function($scope, $window, $modal, barangFactory) {
+        $scope.module = "barang";
+        $scope.fields = [{
+            "name": "kode",
+            "type": "string",
+            "header": "Kode"
+        }, {
+            "name": "kategori",
+            "type": "string",
+            "header": "kategori"
+        }, {
+            "name": "nama",
+            "type": "string",
+            "header": "Nama"
+        }, {
+            "name": "alias",
+            "type": "string",
+            "header": "Alias"
+        }, {
+            "name": "satuan",
+            "type": "string",
+            "header": "Satuan"
+        }];
+        $scope.Math = window.Math;
+        $scope.sort = {
+            "field": "kode",
+            "order": false
+        };
+        $scope.load = function() {
+            $scope.barangs = barangFactory.query();
+            $scope.items = $scope.barangs;
+        };
+        $scope.load();
+        $scope.delete = function(barang) {
+            var confirmDelete = $window.confirm("Apakah Anda Yakin?");
+            if (confirmDelete) {
+                barang.$delete(function() {
+                    $scope.load();
+                    $scope.close();
+                });
+            }
+        };
+        $scope.openCreate = function() {
+            $scope.close();
+            var modalInstance = $modal.open({
+                templateUrl: "modules/barang/views/form-barang.views.html",
+                size: "md",
+                backdrop: "static",
+                controller: "createBarangController"
+            });
+            modalInstance.result.then(function() {
+                $scope.load();
+            });
+        };
+        $scope.openRead = function(barang) {
+            $scope.close();
+            $scope.barang = barang;
+            modalInstance = $modal.open({
+                templateUrl: "modules/barang/views/detail-barang.views.html",
+                size: "md",
+                backdrop: "static",
+                scope: $scope
+            });
+            modalInstance.result.then({}, function(reason) {
+                if (reason == "update") {
+                    $scope.openUpdate(barang);
+                }
+            });
+        };
+        $scope.openUpdate = function(barang) {
+            $scope.close();
+            $scope.newForm = false;
+            $scope.barangOld = angular.copy(barang);
+            $scope.barang = angular.copy(barang);
+            $scope.modalInstance = $modal.open({
+                templateUrl: "modules/barang/views/form-barang.views.html",
+                size: "md",
+                backdrop: "static",
+                controller: "createBarangController"
+            });
+            modalInstance.result.then(function() {
+                $scope.load();
+            });
+        };
+        $scope.openUpdate = function(barang) {
+            $scope.close();
+            var modalInstance = $modal.open({
+                templateUrl: "modules/barang/views/form-barang.views.html",
+                size: "md",
+                backdrop: "static",
+                controller: "updateBarangController",
+                resolve: {
+                    barang: function() {
+                        return barang;
+                    }
+                }
+            });
+            modalInstance.result.then(function() {
+                $scope.load();
+            });
+        };
+        $scope.createPermintaanBarang = function() {
+            $scope.newForm = true;
+            $scope.close();
+            $scope.modalInstance = $modal.open({
+                templateUrl: "modules/permintaanbarang/views/form-permintaanbarang.views.html",
+                size: "lg",
+                backdrop: "static",
+                controller: "permintaanBarangController",
+                scope: $scope
+            });
+            $scope.close();
+        };
+    })
+    .controller("createBarangController", function($scope, satuanGudangFactory, kategoriBarangFactory, barangFactory) {
+        $scope.newForm = true;
         $scope.barang = new barangFactory();
-    };
-    $scope.load = function() {
         $scope.kategoriBarangs = kategoriBarangFactory.query();
         $scope.satuanGudangs = satuanGudangFactory.query();
-        $scope.barangs = barangFactory.query();
-        $scope.items = $scope.barangs;
-        $scope.new();
-    };
-    $scope.load();
-    $scope.create = function() {
-        $scope.barang.$save(function() {
-            $scope.load();
-            $scope.close();
-        });
-    };
-    $scope.update = function() {
-        $scope.barang.$update(function() {
-            $scope.load();
-            $scope.close();
-        });
-    };
-    $scope.delete = function(barang) {
-        var confirmDelete = $window.confirm('Apakah Anda Yakin?');
-        if (confirmDelete) {
-            barang.$delete(function() {
-                $scope.load();
-                $scope.close();
+        $scope.create = function() {
+            $scope.barang.$save(function() {
+                $scope.$close();
             });
-        }
-    };
-    $scope.openCreate = function() {
-        $scope.close();
-        $scope.newForm = true;
-        $scope.new();
-        $scope.modalInstance = $modal.open({
-            templateUrl: 'modules/barang/views/form-barang.views.html',
-            size: 'md',
-            backdrop: 'static',
-            scope: $scope
-        });
-    };
-    $scope.openRead = function(barang) {
-        $scope.close();
+        };
+    })
+    .controller("updateBarangController", function($scope, satuanGudangFactory, kategoriBarangFactory, barangFactory, barang) {
+        $scope.barang = barang;
         $scope.newForm = false;
-        $scope.barang = angular.copy(barang);
-        $scope.modalInstance = $modal.open({
-            templateUrl: 'modules/barang/views/detail-barang.views.html',
-            size: 'md',
-            backdrop: 'static',
-            scope: $scope
-        });
-    };
-    $scope.openUpdate = function(barang) {
-        $scope.close();
-        $scope.newForm = false;
-        $scope.barangOld = angular.copy(barang);
-        $scope.barang = angular.copy(barang);
-        $scope.modalInstance = $modal.open({
-            templateUrl: 'modules/barang/views/form-barang.views.html',
-            size: 'md',
-            backdrop: 'static',
-            scope: $scope
-        });
-    };
-    $scope.createPermintaanBarang = function() {
-        $scope.newForm = true;
-        $scope.close();
-        $scope.modalInstance = $modal.open({
-            templateUrl: 'modules/permintaanbarang/views/form-permintaanbarang.views.html',
-            size: 'lg',
-            backdrop: 'static',
-            controller: 'permintaanBarangController',
-            scope: $scope
-        });
-        $scope.close();
-    };
-}]);
+        $scope.kategoriBarangs = kategoriBarangFactory.query();
+        $scope.satuanGudangs = satuanGudangFactory.query();
+        $scope.update = function() {
+            $scope.barang.$update(function() {
+                $scope.$close();
+            });
+        };
+    });
