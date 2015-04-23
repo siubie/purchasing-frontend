36faca51191ba8416e7d2c6e418988908f45c148
Disyam Adityana <disyam.adityana@gmail.com>
2015-04-23 15:56:32 +0700

    setting web service production di semua modul

--- modules/katalogbarang/services/katalogbarang.services.js
+++ modules/katalogbarang/services/katalogbarang.services.js
@@ -1,6 +1,6 @@
 angular.module('katalogBarang.services', []).factory('katalogBarangFactory', ['$resource', function($resource) {
     return $resource('http://dev.disyam.com:3000/purchasing/katalogbarang/:id1/:id2', {}, {
-        // return $resource('http://purchasing.behaestex.co.id:8080/PurchasingApp/purchasing/lpb/:id', {}, {
+        // return $resource('http://purchasing.behaestex.co.id:8080/PurchasingApp/purchasing/katalog/:id', {}, {
         'get': {
             method: 'GET',
             params: {
